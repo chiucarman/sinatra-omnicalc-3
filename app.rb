e4d7a499479a6ea3b63d879f9a43f1a5a76f5632
Carman Chiu <101212528+chiucarman@users.noreply.github.com>
2023-09-20 22:39:12 +0000

    bundle install "http" gem to parse api response

--- app.rb
+++ app.rb
@@ -1,5 +1,6 @@
 require "sinatra"
 require "sinatra/reloader"
+require "http"
 
 get("/") do
   "
@@ -9,11 +10,19 @@ get("/") do
 end
 
 get("/umbrella") do
-  erb(:umbrella_form)
+  erb(:umbrella_form )
 end
 
 get("/process_umbrella") do
   @user_location = params.fetch("user_loc")
+
+  gmaps_url = "https://maps.googleapis.com/maps/api/geocode/json?address=Merchandise%20Mart%20Chicago&key=AIzaSyDKz4Y3bvrTsWpPRNn9ab55OkmcwZxLOHI"
+
+  @raw_response = HTTP.get(gmaps_url).to_s
+
+  @parsed_response = JSON.parse(@raw_response)
+
+  @loc_hash = @parsed_response.dig("results", 0, "geometry", "location")
 
   erb(:umbrella_results)
 end
