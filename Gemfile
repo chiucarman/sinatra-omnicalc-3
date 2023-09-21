99fd26ae2037ad8f586621b39f4a377d2c4c8135
Carman Chiu <101212528+chiucarman@users.noreply.github.com>
2023-09-21 00:54:57 +0000

    idk whats going on so i added better_errors

--- Gemfile
+++ Gemfile
@@ -6,6 +6,8 @@ ruby "3.2.1"
 gem "sinatra"
 gem "sinatra-contrib"
 gem "http"
+gem "better_errors"
+gem "binding_of_caller"
 
 # Use Puma as the app server
 gem "puma", "~> 5.0"
--- app.rb
+++ app.rb
@@ -2,6 +2,13 @@ require "sinatra"
 require "sinatra/reloader"
 require "http"
 require "sinatra/cookies"
+require "better_errors"
+require "binding_of_caller"
+
+#Need this configuration for better_errors
+use(BetterErrors::Middleware)
+BetterErrors.application_root = __dir__
+BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
 
 get("/") do
   "
@@ -16,13 +23,15 @@ get("/umbrella") do
 end
 
 post("/process_umbrella") do
+  # location info
   @user_location = params.fetch("user_loc")
 
   url_encoded_string = @user_location.gsub(" ", "+")
 
   gmaps_key = ENV.fetch("GMAPS_KEY")
+  # @gmaps_url = "https://maps.googleapis.com/maps/api/geocode/json?address=#{url_encoded_string}&key=#{gmaps_key}"
 
-  gmaps_url = "https://maps.googleapis.com/maps/api/geocode/json?address=#{url_encoded_string}&key=#{gmaps_key}"
+  @gmaps_url = "https://maps.googleapis.com/maps/api/geocode/json?address=" + @user_location + "&key=" + ENV.fetch("GMAPS_KEY")
 
   @raw_response = HTTP.get(gmaps_url).to_s
 
@@ -30,8 +39,12 @@ post("/process_umbrella") do
 
   @loc_hash = @parsed_response.dig("results", 0, "geometry", "location")
 
-  @latitude = @loc_hash.fetch("lat")
-  @longitude = @loc_hash.fetch("lng")
+  # @latitude = @loc_hash.fetch("lat")
+  # @longitude = @loc_hash.fetch("lng")
+
+  # weather info
+  # weather_key = ENV.fetch("PIRATE_WEATHER_KEY")
+  # @weather_url = "https://api.pirateweather.net/forecast/#{weather_key}/#{@latitude},#{@longitude}"
 
   # cookies["last_location"] = @user_location
   # cookies["last_lat"] = @latitude
